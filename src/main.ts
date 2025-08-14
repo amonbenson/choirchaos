49d4e6505dee91a902676eddd46d6aad0498c8e3
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2025-08-14 18:49:50 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+import App from "./App.vue";
+import "./main.css";
+
+const app = createApp(App)
+app.use(createPinia());
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      prefix: "p",
+      darkModeSelector: "system",
+      cssLayer: {
+        name: "primevue",
+        order: "theme, base, primevue",
+      },
+    },
+  },
+});
+app.mount('#app')
