23be00db3f0dddb6ff4edc525ddf77f557966f97
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2025-08-15 22:35:54 +0200

    added more routing options

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { RouterView, useRouter } from "vue-router";
+import { RouterView, useRoute, useRouter } from "vue-router";
 import { login } from "./pocketbase/auth";
 import { onMounted } from "vue";
 
 const router = useRouter();
+const route = useRoute();
 
 onMounted(async () => {
   // auto-login
@@ -11,6 +12,11 @@ onMounted(async () => {
     console.info(`Auto-login activated. Trying to log in with email '${import.meta.env["VITE_PB_AUTOLOGIN_EMAIL"]}'`);
     const res = await login(import.meta.env["VITE_PB_AUTOLOGIN_EMAIL"], import.meta.env["VITE_PB_AUTOLOGIN_PASS"]);
     console.info(`Logged in as '${res.record.name}'`);
+  }
+
+  // auto-select show
+  if (route.path === "/" && import.meta.env["VITE_PB_AUTOLOGIN_SHOW"]) {
+    router.push(`/show/${import.meta.env["VITE_PB_AUTOLOGIN_SHOW"]}`);
   }
 });
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,3 @@
-<script lang="ts">
-import { onMounted } from "vue";
-import { useRouter } from "vue-router";
-
-const router = useRouter();
-
-onMounted(() => {
-  // auto-select show
-  // if (import.meta.env["VITE_PB_AUTOLOGIN_SHOW"]) {
-  //   router.push(`/show/${import.meta.env["VITE_PB_AUTOLOGIN_SHOW"]}`);
-  // }
-});
-</script>
-
 <template>
   Home
 </template>
